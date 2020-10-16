6490abe075b89c6258120e1a76dfb8717a68e14b
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-16 12:45:01 -0700

    changed placeholders

--- data.js
+++ data.js
@@ -1,137 +1,111 @@
 export default [
     {
-        id: 'monsters',
+        id: 'forest',
         title: 'Investigate the dim red glow coming from the forest.',
         map: {
             top: '89%',
             left: '44%'
         },
-        image: 'monsters.jpg',
+        image: '',
         description: `
-        You enter the quest chamber only to be confronted by a hoard of
-        monsters. And they look hungry. What do you do?
+        Something something
     `,
         choices: [{
-            id: 'negotiate',
-            description: 'Negotiate with them',
+            id: 'greet',
+            description: 'Something something',
             result: `
-            Knowing the monsters are not too bright, you offer to go buy them all
-            turkey dinners from the village pub. They give you 35 gold for meals
-            that will never be delivered. I hope you can live with yourself. 
+            Something something 
         `,
             sanity: 0,
             eucharist: 35
         }, {
             id: 'fight',
-            description: 'Fiiiiiggghhhttt!',
+            description: 'Something something',
             result: `
-            Brandishing your sword you let out a warrior's cry and charge into the monsters
-            hacking and slashing. Before long you stand panting gazing across the bodies of
-            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
-            find 50 gold.
+            Something something
         `,
             sanity: -30,
             eucharist: 50
         }, {
             id: 'run',
-            description: 'Run away like good Sir Robin',
+            description: 'Something something',
             result: `
-            As you make a dash for the door a giant spider descends and take a bite of flesh,
-            causing 50 hp damage.
+            Something something
         `,
             sanity: -50,
             eucharist: 0
         }]
     },
     {
-        id: 'dragon',
+        id: 'church',
         title: 'Go back to the church.',
         map: {
             top: '17%',
             left: '37%'
         },
-        image: 'dragon.jpg',
-        audio: 'dragon.wav',
-        action: 'dragon-growl.aiff',
+        image: '',
+        audio: '',
+        action: '',
         description: `
-        You travel to a nearby village you have heard is being
-        terrorized by a dragon. Sure enough as you rent a room
-        in a local inn, you go outside and see the dragon about
-        to lay seige! What do you do?
+        Something something
     `,
         choices: [{
-            id: 'run',
-            description: 'Get the hell out of the village',
+            id: 'listen',
+            description: 'Something something',
             result: `
-            You high tail it in the opposite direction. Luckily,
-            in the panic you find a bag on the ground with 15 gold.
-            Unluckily, you trip over a discarded wagon wheel on your
-            way out of town and take 40 hp damage. 
+            Something something
         `,
             sanity: -35,
             eucharist: 35
         }, {
             id: 'fight',
-            description: 'Fiiiiiggghhhttt!',
+            description: 'Something something',
             result: `
-            You attempt to charge towards the dragon, who sees you approach
-            and let's loose a fireball. You wake up the next morning and the
-            village has been completely burned to the ground.
-            Oh, and you take 45 hp damage.
+            Something something
         `,
             sanity: -45,
             eucharist: 0
         }, {
-            id: 'archer',
-            description: 'Emulate that guy from LOR who shot an arrow',
+            id: 'burn',
+            description: 'Something something',
             result: `
-            Inspired by the legend of Bard the Bowman, you notice a
-            stunned archer standing nearby and take their bow and quiver,
-            climb to the top of a tall tower and take aim. On the dragon's
-            next pass you steady your aim and let one fly. Amazingly,
-            you strike the dragon in the eye, piercing into the brain and
-            killing the dragon instantly. The villagers declare you their hero
-            and award you 90 gold.
+            Something something
         `,
             sanity: 0,
-            eucharist: 90
+            eucharist: 20
         }]
     },
     {
-        id: 'treasure',
+        id: 'home',
         title: 'Knock on the door of the warmly lit home.',
         map: {
             top: '31%',
             left: '5%'
         },
-        prerequisites: ['dragon', 'monsters'],
-        image: 'treasure-chests.png',
-        audio: 'treasure-chests.wav',
-        action: 'chest-opening.wav',
+        prerequisites: [],
+        image: 'something.png',
+        audio: '',
+        action: '',
         description: `
-        As you enter the quest chamber you notice three chests before you.
-        Just as you start to imagine the wealth, you see a giant serpent
-        emerge from the back of the chamber. You'll need to make a run for it,
-        but you have time to open one chest before you take off. Which one 
-        do you choose?
+        Something something
     `,
         choices: [{
-            id: 'wooden',
-            description: 'A Wooden Chest',
-            result: 'You grab 40 gold pieces!',
+            id: 'fakeIt',
+            description: 'Fake it till you make it!',
+            result: 'You lose some Sanity but feel like you may have avoided something terrible.',
             sanity: 0,
             eucharist: 40
         }, {
-            id: 'golden',
-            description: 'A Golden Chest',
-            result: 'Oh no! The chest is booby trapped with poison and you take 50 hp damage',
+            id: 'flip',
+            description: 'Flip the table and run away.',
+            result: 'Something something',
             sanity: -50,
             eucharist: 0
         }, {
-            id: 'jeweled',
+            id: 'You ask to use the bathroom.',
             description: 'A Jeweled Chest',
-            result: 'A warm light engulfs you and you gain 35 hp',
-            sanity: 35,
+            result: 'Something something',
+            sanity: 30,
             eucharist: 0
         }]
     }
